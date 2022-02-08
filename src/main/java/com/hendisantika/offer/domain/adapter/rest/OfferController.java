c6680782075198097bf46189ab76f251f6c4e467
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-09 05:59:10 +0700

    #61 - Add createOffer in OfferController

--- src/main/java/com/hendisantika/offer/domain/adapter/rest/OfferController.java
+++ src/main/java/com/hendisantika/offer/domain/adapter/rest/OfferController.java
@@ -2,7 +2,17 @@ package com.hendisantika.offer.domain.adapter.rest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +32,16 @@ class OfferController {
     private static final String OFFER_PATH_PARAM = "/{offerUUID}";
     private static final String OFFER_URL = OFFERS_URL + OFFER_PATH_PARAM;
     private final OfferServiceRest offerService;
+
+    @PostMapping(
+            path = OFFERS_URL,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<Void> createOffer(@RequestBody @Valid CreateOfferRequest request) {
+        log.info("POST request on [{}]", OFFERS_URL);
+        String createdBy = "rest-api-user"; // Can be replaced with Spring Security user
+        UUID offerUUID = offerService.createOffer(request, createdBy, LocalDateTime.now());
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(OFFER_PATH_PARAM).buildAndExpand(offerUUID).toUri();
+        return ResponseEntity.created(location).build();
+    }
 }
