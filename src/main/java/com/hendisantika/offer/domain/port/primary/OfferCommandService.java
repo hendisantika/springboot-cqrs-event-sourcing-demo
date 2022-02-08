fb401c6bd2908a86a9025c4d84527715b54c7c3f
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:32:33 +0700

    #27 - Add OfferCommandService

---
+++ src/main/java/com/hendisantika/offer/domain/port/primary/OfferCommandService.java
@@ -0,0 +1,21 @@
+package com.hendisantika.offer.domain.port.primary;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 20.32
+ * To change this template use File | Settings | File Templates.
+ */
+public interface OfferCommandService {
+    UUID createOffer(CreateOfferCommand command);
+
+    void updateOffer(UpdateOfferCommand command);
+
+    void markOfferAsPaid(MarkOfferAsPaidCommand command);
+}
