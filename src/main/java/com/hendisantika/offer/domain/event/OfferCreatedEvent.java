710b83bda3dbdba0445fc7eaebc4e28d84ccf7b2
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 19:59:30 +0700

    #11 - Add OfferCreatedEvent

---
+++ src/main/java/com/hendisantika/offer/domain/event/OfferCreatedEvent.java
@@ -0,0 +1,48 @@
+package com.hendisantika.offer.domain.event;
+
+import com.hendisantika.offer.domain.infrastructure.event.BaseOfferEvent;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 19.53
+ * To change this template use File | Settings | File Templates.
+ */
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class OfferCreatedEvent extends BaseOfferEvent {
+    public static final String TYPE = "offer.created";
+
+    UUID offerUUID;
+    String productName;
+    BigDecimal amount;
+    LocalDateTime createdAt;
+    String createdBy;
+
+    public OfferCreatedEvent(UUID offerUUID, String productName, BigDecimal amount, LocalDateTime createdAt, String createdBy) {
+        this.offerUUID = offerUUID;
+        this.productName = productName;
+        this.amount = amount;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+    }
+
+    @Override
+    public String type() {
+        return TYPE;
+    }
+
+    @Override
+    public UUID offerUUID() {
+        return offerUUID;
+    }
+}
