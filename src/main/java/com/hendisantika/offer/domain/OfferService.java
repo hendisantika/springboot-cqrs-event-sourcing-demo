f33d8719687b2fae4d9d63e25f88fb44a388689c
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:39:28 +0700

    #33 - Add store in OfferService

--- src/main/java/com/hendisantika/offer/domain/OfferService.java
+++ src/main/java/com/hendisantika/offer/domain/OfferService.java
@@ -1,9 +1,12 @@
 package com.hendisantika.offer.domain;
 
+import com.hendisantika.offer.domain.infrastructure.event.OfferEvent;
 import com.hendisantika.offer.domain.port.secondary.OfferStore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +24,12 @@ import org.springframework.stereotype.Component;
 class OfferService {
     private final OfferStore offerStore;
     private final DomainEventPublisher eventPublisher;
+
+    void store(Offer offer) {
+        List<OfferEvent> pendingEvents = offer.getPendingEvents();
+        int noOfEvents = pendingEvents.size();
+        pendingEvents.forEach(this::saveAndPublish);
+        offer.markEventsAsCommitted();
+        log.info("Offer with UUID [{}] stored, [{}] pending events saved and published", offer.getUuid(), noOfEvents);
+    }
 }
