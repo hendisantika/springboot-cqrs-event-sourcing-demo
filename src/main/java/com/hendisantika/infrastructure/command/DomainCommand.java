a3edab64f6c6a0bc1f4316837d398f1d2a9c1e41
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:34:36 +0700

    #29 - Add DomainCommand

---
+++ src/main/java/com/hendisantika/infrastructure/command/DomainCommand.java
@@ -0,0 +1,14 @@
+package com.hendisantika.infrastructure.command;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 20.34
+ * To change this template use File | Settings | File Templates.
+ */
+public class DomainCommand {
+}
