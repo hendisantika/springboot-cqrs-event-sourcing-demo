5420d3f1b5f857b5ad92d9e306eb7f2a8c7a389c
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:30:50 +0700

    #25 - Add OfferService

---
+++ src/main/java/com/hendisantika/offer/domain/OfferService.java
@@ -0,0 +1,23 @@
+package com.hendisantika.offer.domain;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 20.30
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+class OfferService {
+    private final OfferStore offerStore;
+    private final DomainEventPublisher eventPublisher;
+}
