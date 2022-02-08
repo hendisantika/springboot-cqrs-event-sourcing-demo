83ca0cdf84b1f31d272e0d9ac43b1bb0069f9dcb
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:01:44 +0700

    #12 - Add OfferState

---
+++ src/main/java/com/hendisantika/offer/domain/OfferState.java
@@ -0,0 +1,15 @@
+package com.hendisantika.offer.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 20.01
+ * To change this template use File | Settings | File Templates.
+ */
+enum OfferState {
+    NEW, PAID
+}
