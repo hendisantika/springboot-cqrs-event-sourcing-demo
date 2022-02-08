1652797ad102992e245c9e0cda303b06ebd0d7a4
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:46:10 +0700

    #40 - Add updateOffer in CommandService

--- src/main/java/com/hendisantika/offer/domain/CommandService.java
+++ src/main/java/com/hendisantika/offer/domain/CommandService.java
@@ -2,6 +2,7 @@ package com.hendisantika.offer.domain;
 
 import com.hendisantika.infrastructure.uuid.UUIDGenerator;
 import com.hendisantika.offer.domain.command.CreateOfferCommand;
+import com.hendisantika.offer.domain.command.UpdateOfferCommand;
 import com.hendisantika.offer.domain.port.primary.OfferCommandService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,5 +34,13 @@ class CommandService implements OfferCommandService {
         store(offer);
         log.info("Created new offer with UUID [{}]", offerUUID);
         return offerUUID;
+    }
+
+    @Override
+    public void updateOffer(UpdateOfferCommand command) {
+        Offer offer = loadOffer(command.getOfferUUID());
+        offer.update(command.getProductName(), command.getAmount(), command.getUpdatedAt(), command.getUpdatedBy());
+        store(offer);
+        log.info("Updated offer with UUID [{}]", command.getOfferUUID());
     }
 }
