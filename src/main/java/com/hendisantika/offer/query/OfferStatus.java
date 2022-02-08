50a7d75d83185c766e830316a4eec7ac9e31eda4
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:21:45 +0700

    #16 - Add OfferStatus

---
+++ src/main/java/com/hendisantika/offer/query/OfferStatus.java
@@ -0,0 +1,16 @@
+package com.hendisantika.offer.query;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 20.21
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OfferStatus {
+    NEW,
+    PAID
+}
