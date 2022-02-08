92abb22526e2200173867c84664ff0ef6d93cddd
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:52:45 +0700

    #49 - Add saveEvents in EventStore

--- src/main/java/com/hendisantika/infrastructure/event/EventStore.java
+++ src/main/java/com/hendisantika/infrastructure/event/EventStore.java
@@ -1,9 +1,11 @@
 package com.hendisantika.infrastructure.event;
 
+import com.hendisantika.infrastructure.db.EventDescriptor;
 import com.hendisantika.infrastructure.db.EventStream;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,4 +22,11 @@ import java.util.UUID;
 @Repository
 public interface EventStore extends JpaRepository<EventStream, Long> {
     Optional<EventStream> findByUuid(UUID uuid);
+
+    default void saveEvents(UUID aggregateId, List<EventDescriptor> events) {
+        final EventStream eventStream = findByUuid(aggregateId)
+                .orElseGet(() -> new EventStream(aggregateId));
+        eventStream.addEvents(events);
+        save(eventStream);
+    }
 }
