1de684bd205c409a71668ca4f4bab5f995b5244a
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:37:27 +0700

    #31 - Add OfferDomainCommand

---
+++ src/main/java/com/hendisantika/offer/domain/infrastructure/command/OfferDomainCommand.java
@@ -0,0 +1,16 @@
+package com.hendisantika.offer.domain.infrastructure.command;
+
+import com.hendisantika.infrastructure.command.DomainCommand;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 20.36
+ * To change this template use File | Settings | File Templates.
+ */
+public interface OfferDomainCommand extends DomainCommand {
+}
