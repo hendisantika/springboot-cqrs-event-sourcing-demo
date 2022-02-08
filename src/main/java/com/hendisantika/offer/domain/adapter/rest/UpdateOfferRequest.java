b375327afd5c7b3cc1affd2c73cd3282f6783ad1
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:56:13 +0700

    #53 - Add UpdateOfferRequest

---
+++ src/main/java/com/hendisantika/offer/domain/adapter/rest/UpdateOfferRequest.java
@@ -0,0 +1,27 @@
+package com.hendisantika.offer.domain.adapter.rest;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 20.55
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+class UpdateOfferRequest {
+    @NotEmpty
+    private String productName;
+    @NotNull
+    private BigDecimal amount;
+}
