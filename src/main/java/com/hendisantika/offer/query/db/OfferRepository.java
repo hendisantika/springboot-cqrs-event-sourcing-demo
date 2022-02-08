b59f21df26a9640f668c828adc7f200dea4f6703
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:25:43 +0700

    #20 - Add OfferRepository

---
+++ src/main/java/com/hendisantika/offer/query/db/OfferRepository.java
@@ -0,0 +1,22 @@
+package com.hendisantika.offer.query.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 20.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+interface OfferRepository extends JpaRepository<Offer, Long> {
+    Optional<Offer> findByUuid(UUID uuid);
+}
