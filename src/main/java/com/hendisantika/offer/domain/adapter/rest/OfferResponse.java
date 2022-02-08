2662327c95d5a766fb49b0d40787b275391322c1
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-08 20:57:35 +0700

    #56 - Add OfferResponse

---
+++ src/main/java/com/hendisantika/offer/domain/adapter/rest/OfferResponse.java
@@ -0,0 +1,35 @@
+package com.hendisantika.offer.domain.adapter.rest;
+
+import com.hendisantika.offer.query.OfferStatus;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-cqrs-event-sourcing-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/22
+ * Time: 20.57
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+class OfferResponse {
+    private UUID uuid;
+    private String productName;
+    private BigDecimal amount;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+    private LocalDateTime paidAt;
+    private String paidBy;
+    private OfferStatus status;
+    private Long version;
+}
